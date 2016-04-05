a346a21b02713ae91e25672551217cf1743f586d
vurdalakov <vurdalakov@gmail.com>
2016-04-05 15:20:01 +0300

    Main window position and size saved between sessions

--- src/WebSequenceDiagrams/MainViewModel.cs
+++ src/WebSequenceDiagrams/MainViewModel.cs
@@ -116,6 +116,82 @@
             }
         }
 
+        private Int32 _mainWindowLeft = 64;
+        public Int32 MainWindowLeft
+        {
+            get
+            {
+                return this._mainWindowLeft;
+            }
+            set
+            {
+                if (value != this._mainWindowLeft)
+                {
+                    this._mainWindowLeft = value;
+                    this.OnPropertyChanged(() => this.MainWindowLeft);
+
+                    this._settings.Set("MainWindowLeft", this._mainWindowLeft);
+                }
+            }
+        }
+
+        private Int32 _mainWindowTop = 64;
+        public Int32 MainWindowTop
+        {
+            get
+            {
+                return this._mainWindowTop;
+            }
+            set
+            {
+                if (value != this._mainWindowTop)
+                {
+                    this._mainWindowTop = value;
+                    this.OnPropertyChanged(() => this.MainWindowWidth);
+
+                    this._settings.Set("MainWindowTop", this._mainWindowTop);
+                }
+            }
+        }
+
+        private Int32 _mainWindowWidth = 1024;
+        public Int32 MainWindowWidth
+        {
+            get
+            {
+                return this._mainWindowWidth;
+            }
+            set
+            {
+                if (value != this._mainWindowWidth)
+                {
+                    this._mainWindowWidth = value;
+                    this.OnPropertyChanged(() => this.MainWindowWidth);
+
+                    this._settings.Set("MainWindowWidth", this._mainWindowWidth);
+                }
+            }
+        }
+
+        private Int32 _mainWindowHeight = 768;
+        public Int32 MainWindowHeight
+        {
+            get
+            {
+                return this._mainWindowHeight;
+            }
+            set
+            {
+                if (value != this._mainWindowHeight)
+                {
+                    this._mainWindowHeight = value;
+                    this.OnPropertyChanged(() => this.MainWindowHeight);
+
+                    this._settings.Set("MainWindowHeight", this._mainWindowHeight);
+                }
+            }
+        }
+
         public ThreadSafeObservableCollection<ErrorViewModel> Errors { get; private set; }
 
         private PermanentSettings _settings;
@@ -134,11 +210,16 @@
 
             this.Errors = new ThreadSafeObservableCollection<ErrorViewModel>();
 
-            this._wsdScript = "title Simple Sequence Diagram\r\nClient->Server: request image\r\nServer-> Client: return image";
-            this.Refresh();
-
             this._timer = new Timer(1000);
             this._timer.Elapsed += OnTimerElapsed;
+
+            this.MainWindowLeft = this._settings.Get("MainWindowLeft", 64);
+            this.MainWindowTop = this._settings.Get("MainWindowTop", 64);
+            this.MainWindowWidth = this._settings.Get("MainWindowWidth", 1024);
+            this.MainWindowHeight = this._settings.Get("MainWindowHeight", 768);
+
+            this._wsdScript = "title Simple Sequence Diagram\r\nClient->Server: request image\r\nServer-> Client: return image";
+            this.Refresh();
         }
 
         public ICommand ExitCommand { get; private set; }
